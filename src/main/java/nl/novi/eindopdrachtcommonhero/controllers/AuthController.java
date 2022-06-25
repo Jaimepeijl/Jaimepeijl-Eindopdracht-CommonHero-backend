9817523465cac75745693ff412bc62e10cb5cccf
jaimepeijl <j.d.peijl@gmail.com>
2022-06-25 11:49:33 +0200

    Login function toegevoegd

---
+++ src/main/java/nl/novi/eindopdrachtcommonhero/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package nl.novi.eindopdrachtcommonhero.controllers;
+
+import nl.novi.eindopdrachtcommonhero.controllers.dto.AuthDto;
+import nl.novi.eindopdrachtcommonhero.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> signIn(@RequestBody AuthDto authDto){
+        UsernamePasswordAuthenticationToken up =
+                new UsernamePasswordAuthenticationToken(authDto.getUsername(), authDto.getPassword());
+        Authentication auth = authManager.authenticate(up);
+
+        UserDetails ud = (UserDetails) auth.getPrincipal();
+        String token = jwtUtil.generateToken(ud);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .body(token);
+    }
+}
